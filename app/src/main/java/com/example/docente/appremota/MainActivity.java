c7f59a4dd9da3f036a0c124b00f3950a40070da4
David Reyna <reyna_b@upeu.edu.pe>
2016-02-18 08:39:55 -0500

    v2: Se desarrollo el listar Usuarios

--- app/src/main/java/com/example/docente/appremota/MainActivity.java
+++ app/src/main/java/com/example/docente/appremota/MainActivity.java
@@ -8,20 +8,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
- private EditText n1,n2;
+ private EditText n1,n2; ListView lvdatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        lvdatos = (ListView) findViewById(R.id.listaUser);
+        listarUsuarios();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -39,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void datosUsuarios(String nombres, String apellidos, String usuario, String clave){
         AsyncHttpClient client = new AsyncHttpClient();
-        String url = "http://192.168.14.60/LP3Moviles/registro.php?";
+        //String url = "http://192.168.0.22/LP3Moviles/registro.php?";
+        String url = "http://192.168.40.26/LP3Moviles/registro.php";
         String parametros ="Nombres=" +nombres + "&Apellidos=" + apellidos + "&Usuario=" + usuario + "&Clave="+ clave;
         client.post(url + parametros, new AsyncHttpResponseHandler() {
             @Override
@@ -58,7 +67,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void cargarLisview(ArrayList<String> datos){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datos);
+        lvdatos.setAdapter(adapter);
+    }
+    public void listarUsuarios(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://192.168.40.26/LP3Moviles/ListaUsuarios.php";
+        RequestParams  params = new RequestParams();
+        client.get(url, null, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200) {
+                    cargarLisview(getJson(new String(responseBody)));
+                }
+            }
 
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+    public ArrayList<String> getJson(String response){
+        ArrayList<String> lista = new ArrayList<>();
+        try {
+            JSONArray array = new JSONArray(response);
+            String cadena;
+            for(int i=0; i<array.length();i++){
+                cadena = array.getJSONObject(i).getString("usuario");
+                lista.add(cadena);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return lista;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
